a167faaa8014178afaa3dabf8c6095d867b18bac
palakie316-netizen <palakie316@gmail.com>
2026-02-08 02:39:52 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "pay-planner-v9";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE);
+    await cache.addAll(ASSETS);
+    self.skipWaiting();
+  })());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k === CACHE) ? null : caches.delete(k)));
+    self.clients.claim();
+  })());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith((async () => {
+    const cached = await caches.match(event.request);
+    if (cached) return cached;
+    try { return await fetch(event.request); }
+    catch (e) { return caches.match("./index.html"); }
+  })());
+});
